32fe22b1d97b216d2daed0793955cfc93954a9aa
Yoonsub <yoonsub90@gmail.com>
2018-12-05 18:52:53 -0500

    front interface design

--- characterSel.js
+++ characterSel.js
@@ -5,23 +5,38 @@ function characterPage() {
     $("<div>").append(
       $("<p>")
         .attr("id", "welcome")
-        .text("Welcome " + ajaxGetData),
+        .text("Welcome " + ajaxGetData)
+        .css("font-size", "70px")
+        .css("text-align", "center"),
       $("<p>")
         .attr("id", "choose")
-        .text("Please choose your charater"),
+        .text("Please choose your charater")
+        .css("font-size", "30px")
+        .css("text-align", "center")
+        .css("margin-top", "30px"),
+
       $("<div>")
         .addClass("charGroup")
         .append(
-          $("<img src = 'assets/ship_1.png'>").addClass("charImage"),
-          $("<img src = 'assets/ship_2.png'>").addClass("charImage"),
-          $("<img src = 'assets/ship_3.png'>").addClass("charImage"),
-          $("<img src = 'assets/ship_4.png'>").addClass("charImage"),
-          $("<img src = 'assets/ship_5.png'>").addClass("charImage"),
-          $("<img src = 'assets/ship_6.png'>").addClass("charImage")
+          $("<div>")
+            .addClass("row s3")
+            .append(
+              $("<img src = 'assets/ship_1.png'>").addClass("charImage col s1"),
+              $("<img src = 'assets/ship_2.png'>").addClass("charImage col s1"),
+              $("<img src = 'assets/ship_3.png'>").addClass("charImage col s1")
+            ),
+          $("<div>")
+            .addClass("row s3")
+            .append(
+              $("<img src = 'assets/ship_4.png'>").addClass("charImage col s1"),
+              $("<img src = 'assets/ship_5.png'>").addClass("charImage col s1"),
+              $("<img src = 'assets/ship_6.png'>").addClass("charImage col s1")
+            )
         ),
       $("<button>")
         .attr("id", "selectButton")
         .text("Select")
+        .addClass("btn waves-effect waves-light")
     )
   );
 }
--- rooms.js
+++ rooms.js
@@ -1,49 +1,65 @@
 function roomPage() {
+  const number = "(0/2)";
   $("#canvas").empty();
   const ajaxGetData = "Chris";
   $("#canvas").append(
     $("<div>").append(
       $("<p>")
         .attr("id", "welcome")
-        .text("Welcome " + ajaxGetData),
+        .text("Welcome " + ajaxGetData)
+        .css("font-size", "70px")
+        .css("text-align", "center"),
       $("<p>")
         .attr("id", "choose")
-        .text("Which room would you like to play in?"),
+        .text("Which room would you like to play in?")
+        .css("font-size", "30px")
+        .css("text-align", "center")
+        .css("margin-top", "30px"),
       $("<div>")
         .addClass("roomGroup")
         .append(
           $("<div>")
-            .addClass("roomBorder")
+            .addClass("row")
             .append(
-              $("<p>").text("Room number 1"),
-              $("<p>").text("0/2"),
-              $("<button>").text("This One?")
+              $("<div>")
+                .addClass("roomBorder col s4")
+                .append(
+                  $("<button>")
+                    .addClass("btn btn-large waves-effect waves-teal btn-flat")
+                    .text(`Room number 1` + `${number}`)
+                ),
+              $("<div>")
+                .addClass("roomBorder col s4")
+                .append(
+                  $("<button>")
+                    .addClass("btn btn-large waves-effect waves-teal btn-flat")
+                    .text(`Room number 2` + `${number}`)
+                )
             ),
+
           $("<div>")
-            .addClass("roomBorder")
+            .addClass("row")
             .append(
-              $("<p>").text("Room number 2"),
-              $("<p>").text("0/2"),
-              $("<button>").text("This One?")
-            ),
-          $("<div>")
-            .addClass("roomBorder")
-            .append(
-              $("<p>").text("Room number 3"),
-              $("<p>").text("0/2"),
-              $("<button>").text("This One?")
-            ),
-          $("<div>")
-            .addClass("roomBorder")
-            .append(
-              $("<p>").text("Room number 4"),
-              $("<p>").text("0/2"),
-              $("<button>").text("This One?")
+              $("<div>")
+                .addClass("roomBorder col s4")
+                .append(
+                  $("<button>")
+                    .addClass("btn btn-large waves-effect waves-teal btn-flat")
+                    .text(`Room number 3` + `${number}`)
+                ),
+              $("<div>")
+                .addClass("roomBorder col s4")
+                .append(
+                  $("<button>")
+                    .addClass("btn btn-large waves-effect waves-teal btn-flat")
+                    .text(`Room number 4` + `${number}`)
+                )
             )
         ),
       $("<button>")
-        .attr("id", "selectButton")
+        .attr("id", "selectButton2")
         .text("Select")
+        .addClass("btn waves-effect waves-light")
     )
   );
 }
